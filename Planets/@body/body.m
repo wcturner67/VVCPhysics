baec92f176a5e6b1eb49960653c5dab77e673658
William <wcturner67@yahoo.com>
2017-12-18 18:52:31 -0800

    Fixed property dependence

--- Planets/@body/body.m
+++ Planets/@body/body.m
@@ -1,22 +1,81 @@
 classdef body
-    properties (Access = public)
-        pos(1, 3) double
-        vel(1, 3) double
+    %%
+    properties
+        mass(1, 1) double
     end
-    
+    %%
+    properties (Dependent = true)
+        pos
+        vel
+    end
+    %%
     properties (Access = private)
-        acc(1, 3) double
+        posX(1, 1) double {mustBeFinite}
+        posY(1, 1) double {mustBeFinite}
+        posZ(1, 1) double {mustBeFinite}
+        velX(1, 1) double {mustBeFinite}
+        velY(1, 1) double {mustBeFinite}
+        velZ(1, 1) double {mustBeFinite}
     end
     
-    methods (Access = public)
-        % Constructor
-        function obj = body(pos, vel, r)
-            % Input validation
-            
+    methods
+        %% Get methods
+        function pos = get.pos(obj)
+            pos = struct('x', obj.posX, 'y', obj.posY, 'z', obj.posZ);
+        end
+        
+        function vel = get.vel(obj)
+            vel = struct('x', obj.velX, 'y', obj.velY, 'z', obj.velZ);
+        end
+        
+        %% Set methods
+        function obj = set.pos(obj, new)
+            obj.posX = new(1);
+            obj.posY = new(2);
+            if length(new) == 3
+                obj.posZ = new(3);
+            end
+        end
+        
+        function obj = set.vel(obj, new)
+            obj.velX = new(1);
+            obj.velY = new(2);
+            if length(new) == 3
+                obj.velZ = new(3);
+            end
         end
     end
     
+    methods
+        %% Constructor method
+        function obj = body(mass, pos, vel)
+            %
+            %
+            
+            % Input validation
+            if any(size(pos) ~= size(vel))
+                error('Input size mismatch')
+            end
+            
+            % Move input data into output object
+            [numBodies, ~] = size(pos);
+            obj(numBodies) = obj;
+            for k = 1:numBodies
+                % Masses
+                obj(k).mass = mass(k);
+                
+                % Positions
+                obj(k).pos = pos(k, :);
+                
+                % Velocities
+                obj(k).vel = vel(k, :);
+            end
+        end
+        
+        %%
+    end
+    
     methods (Access = private)
-        posNew = posUpdate(body)
+        body = posUpdate(body)
     end
 end
--- Planets/@body/posUpdate.m
+++ Planets/@body/posUpdate.m
@@ -1,4 +1,8 @@
-function posUpdate(body)
+function body = posUpdate(body)
+% Updates the position data of the body vector
+%
 
+% Constants
+G = 6.67408e-11;
 
-
+end
